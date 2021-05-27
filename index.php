750e1018ecd75888f05ac1354205d2834ab2115b
Can Yucel Bal <canyucelbal@gmail.com>
2021-05-27 00:01:31 -0400

    images edited

--- index.php
+++ index.php
@@ -15,27 +15,24 @@ else{
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Welcome to cms</title>
-</head>
-<body>
-<?php include 'template/header.php'; ?> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main.css">
+        <title>Welcome to cms</title>
+    </head>
+    <body>
+        <?php include 'template/header.php'; ?> 
 
-<section class="movie-disp">
-<?php foreach ($getMovies as $movie):?>
-    <div class='movie-item'>
-<img src="images/<?php echo $movie['product_img']; ?>" alt="<?php echo $movie['product_name']; ?> cover image">
-<h2><?php echo $movie['product_name']; ?></h2>
-<a href="details.php?id=<?php echo $movie['product_id']; ?>">More details</a>
-</div>
-    <?php endforeach;?>
-    </section>
-    <?php include './template/footer.php'; ?>
- 
-
-
+        <section class="movie-disp">
+            <?php foreach ($getMovies as $movie):?>
+            <div class='movie-item'>
+                <img src="images/<?php echo $movie['product_img']; ?>" alt="<?php echo $movie['product_name']; ?> cover image">
+                <h2><?php echo $movie['product_name']; ?></h2>
+                <a href="details.php?id=<?php echo $movie['product_id']; ?>">More details</a>
+            </div>
+            <?php endforeach;?>
+        </section>
+            <?php include './template/footer.php'; ?>
 </body>
 </html>
