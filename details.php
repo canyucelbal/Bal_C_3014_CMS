798dbf70acecd071b22e873e8a3b3d14762b69f3
Can Yucel Bal <canyucelbal@gmail.com>
2021-05-28 03:46:09 -0400

    add css, image edit

--- details.php
+++ details.php
@@ -18,33 +18,35 @@ if(isset($_GET['id'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/reset.css">
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
     <link rel="stylesheet" href="css/master.css">
     <title>Welcome to details page</title>
 </head>
 <body>
-
-    <?php include './template/header.php'; ?>     
-    <?php if(!empty($movie)):?>
-    <section class='movie-disp-details'>
-        <div class='movie-item-details'>
-            <h2><?php echo $movie['product_name']; ?></h2>
-            <img src="images/<?php echo $movie['product_img']; ?>" alt="<?php echo $movie['product_name']; ?> Cover image" class="detailsImage">
+    <?php include './template/header.php'; ?>  
+    <div class='container'>
+        <?php if(!empty($movie)):?>
+        <section class='movie-disp-details'>
+            <div class='movie-item-details'>
+                <h2><?php echo $movie['product_name']; ?></h2>
+                <img src="images/<?php echo $movie['product_img']; ?>" alt="<?php echo $movie['product_name']; ?> Cover image" class="detailsImage">
+            </div>
+            <div class='details-info'>
+                <h4>Price: </h4>
+                <p>$<?php echo $movie['product_price']; ?></p>
+                <h4>Colors Available: </h4>
+                <p><?php echo $movie['product_color']; ?></p>
+                <button type="button">Add to Cart!</button>
+            </div>
+        </section>
+        <div class='details-desc'>
+            <h4>Description: </h4>
+            <p><?php echo $movie['product_description']; ?></p>
         </div>
-        <div class='details-info'>
-            <h4>Price: </h4>
-            <p>$<?php echo $movie['product_price']; ?></p>
-            <h4>Colors Available: </h4>
-            <p><?php echo $movie['product_color']; ?></p>
-        </div>
-    </section>
-    <div class='details-desc'>
-        <h4>Description: </h4>
-        <p><?php echo $movie['product_description']; ?></p>
+        <?php else:?>
+        <p>There isnt such a product</p>
+        <?php endif;?>
     </div>
-    <?php else:?>
-    <p>There isnt such a product</p>
-    <?php endif;?>
-
     <?php include './template/footer.php'; ?>
 </body>
 </html>
