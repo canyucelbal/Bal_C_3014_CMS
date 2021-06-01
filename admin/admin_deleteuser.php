28695f891f214ea4f9d1726f0f3b5024d29dca67
Can Yucel Bal <canyucelbal@gmail.com>
2021-05-31 23:57:00 -0400

    add css

--- admin/admin_deleteuser.php
+++ admin/admin_deleteuser.php
@@ -25,9 +25,18 @@ if(isset($_GET['id'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
+    <link rel="stylesheet" href="../css/master.css">
     <title>Delete User</title>
 </head>
 <body>
+<header>
+    <section class="headNavigation">
+        <section class="logoPart">
+        <a href="index.php"> <img src="../images/sportchek-logo.png" alt=""></a>
+        </section>
+    </section>
+    </header>
     <h2>Delete User</h2>
     <?php echo !empty($message) ? $message : ''; ?>
 <a href="index.php">Back to Dashboard page</a>
